43ce17b97446b6f54799476f9ba4d7dd301af7fe
Caroline Souza <carol.silvasouza@hotmail.com>
2024-11-09 12:56:49 -0300

    feat: Foi construída a páginac de sobre

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OrganismHeaderComponent } from '../../shared/components/organisms/organism-header/organism-header.component';
+import { AboutComponent } from '../../shared/components/organisms/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [OrganismHeaderComponent],
+  imports: [OrganismHeaderComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
